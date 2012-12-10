1164650043e55f15da26c3436a85911925a9a173
Sergey Malykh <xronos.i.am@gmail.com>
2012-12-10 14:48:16 +0400

    fixed error occurred when YAML::ENGINE has default value 'psych' but changed to the 'syck' during the loading (gem configatron)

--- lib/delayed/serialization/mongoid.rb
+++ lib/delayed/serialization/mongoid.rb
@@ -2,31 +2,29 @@
 require 'delayed_job'
 require 'mongoid'
 
-if YAML.parser.class.name =~ /syck/i
-  Mongoid::Document.class_eval do
-    yaml_as "tag:ruby.yaml.org,2002:Mongoid"
+Mongoid::Document.class_eval do
+  yaml_as "tag:ruby.yaml.org,2002:Mongoid"
 
-    def self.yaml_new( klass, tag, val )
-      begin
-        klass.unscoped.find( val['_id'] )
-      rescue Mongoid::Errors::DocumentNotFound
-        raise Delayed::DeserializationError
-      end
-    end
-
-    def to_yaml( opts = {} )
-      YAML::quick_emit( self, opts ) do |out|
-        out.map( taguri, to_yaml_style ) do |map|
-          map.add( '_id', self._id.to_s )
-        end
-      end
-    end   
-  end
-else
-  Mongoid::Document.class_eval do
-    def encode_with(coder)
-      coder["attributes"] = @attributes
-      coder.tag = ['!ruby/Mongoid', self.class.name].join(':')
+  def self.yaml_new( klass, tag, val )
+    begin
+      klass.unscoped.find( val['_id'] )
+    rescue Mongoid::Errors::DocumentNotFound
+      raise Delayed::DeserializationError
     end
   end
+
+  def to_yaml( opts = {} )
+    return psych_to_yaml( opts ) if YAML::ENGINE.yamler == 'psych'
+
+    YAML::quick_emit( self, opts ) do |out|
+      out.map( taguri, to_yaml_style ) do |map|
+        map.add( '_id', self._id.to_s )
+      end
+    end
+  end   
+
+  def encode_with( coder )
+    coder[ "_id" ] = self._id.to_s
+    coder.tag = [ '!ruby/Mongoid', self.class.name ].join( ':' )
+  end 
 end
